70140e66a0285e0f93dc5cfb7c6c3d4984026273
ccrismota <ccrismota@gmail.com>
2023-08-13 22:10:46 -0300

    First Commit

---
+++ src/components/LgButton.js
@@ -0,0 +1,26 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import { Button } from "primereact/button";
+
+const LgButton = () => {
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
+  return (
+    !isAuthenticated && (
+      <div className="flex flex-column justify-content-center  mx-8">
+        <Button onClick={() => loginWithRedirect()} style={styles.button} icon="pi pi-google" className="w-full flex flex-row  justify-content-center align-items-center" >
+          <span className="ml-3">Sign In with Google </span>
+        </Button>
+      </div>
+    )
+  )
+};
+
+const styles = {
+    button: {
+        backgroundColor: "#345059",
+        color: "#FFFFFF",
+        with: "100%",
+    },
+}
+
+export default LgButton ;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes, useNavigate, } from "react-router-dom";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import ReactDOM from "react-dom/client";
+import Home from "./components/home";
+import React from "react";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+const AppRoutes = () => {
+  
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth0();
+
+  React.useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/home");
+    }},
+    [isAuthenticated, navigate]);
+    
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
+  <Auth0Provider
+  domain={domain}
+  clientId={clientId}
+  authorizationParams={{
+    redirect_uri: window.location.origin,
+    
+  }}
+  >
+    <React.StrictMode>
+    <Router>
+      <AppRoutes />
+    </Router>
   </React.StrictMode>
+  </Auth0Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default AppRoutes;
